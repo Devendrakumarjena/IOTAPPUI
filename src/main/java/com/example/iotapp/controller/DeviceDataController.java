f50558f495a2e4098a976a8da61fa046b1c71383
Devendra kumar Jena <devendrajenatrichy@gmail.com>
2024-10-14 23:36:20 +0530

    Test

--- src/main/java/com/example/iotapp/controller/DeviceDataController.java
+++ src/main/java/com/example/iotapp/controller/DeviceDataController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/device-data")
+//Testing
 public class DeviceDataController {
 
     private final DeviceDataRepository deviceDataRepository;
